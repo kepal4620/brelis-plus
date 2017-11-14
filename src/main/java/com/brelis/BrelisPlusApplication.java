535f6eee85b75cb928c2451e31e21a73dca2c30e
kepal4620 <karel.tomasek@gmail.com>
2017-11-14 20:36:18 +0100

    Initial commit

---
+++ src/main/java/com/brelis/BrelisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.brelis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrelisPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrelisPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brelis/BrelisPlusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brelis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrelisPlusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
