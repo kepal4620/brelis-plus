68f9ef5d0949894d5e6f714c0c8e9ea8f5cab025
kepal4620 <karel.tomasek@gmail.com>
2017-11-14 23:19:50 +0100

    auth package

--- src/main/java/com/brelis/BrelisPlusApplication.java
+++ src/main/java/com/brelis/BrelisPlusApplication.java
@@ -2,6 +2,8 @@ package com.brelis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BrelisPlusApplication {
@@ -10,4 +12,8 @@ public class BrelisPlusApplication {
 		SpringApplication.run(BrelisPlusApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
